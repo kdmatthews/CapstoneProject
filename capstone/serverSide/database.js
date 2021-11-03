1d5468e0ef6269ea1d22ac0f13c82240e3c46b5b
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-11-03 14:13:00 -0500

    Created create user route. Testing

--- capstone/serverSide/database.js
+++ capstone/serverSide/database.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -11,3 +13,18 @@ const PORT = 3002;
 app.use(express.json());
 app.use(cors())
 
+app.post("/signup", (req, res) => {
+    creds.connect(async() => {
+        try{
+            const data = await creds.query(`INSERT INTO users(name, username, password) VALUES ('${req.body.name}', '${req.body.username}','${req.body.password}')`)
+            res.send('Created new user');
+
+        }
+        catch(err){
+            res.send(err);
+        }
+    })
+});
+
+
+app.listen(PORT, console.log(`I'm listening on ${PORT}`))
