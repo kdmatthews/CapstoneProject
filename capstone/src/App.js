6794f14bd40b619cf2944bf2e00413cd6c8c592f
Kayla Matthews <kdmatthews77@gmail.com>
2021-11-03 11:38:31 -0400

    initial setup

---
+++ capstone/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SOS</h1>
+     <p>Help! I need somebody! Help! Not just anybody!</p>
+    </div>
+  );
+}
+
+export default App;
