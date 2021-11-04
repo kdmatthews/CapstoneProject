e14ebc79896e2d41689135537b269286fdf93c90
Jennifer Neasbitt <jengeo51307@gmail.com>
2021-11-04 15:04:23 -0500

    still trying to get functionality

--- capstone/serverSide/creds.js
+++ capstone/serverSide/creds.js
@@ -1,6 +1,26 @@
 require('dotenv').config()
 const token = process.env.elephant
-const Pool = require("pg").Client
-const creds = new Pool(token)
-
+const pg = require('pg');
+const creds = new pg.Client(token);
 module.exports = creds;
+
+
+
+//or native libpq bindings
+//var pg = require('pg').native
+
+// var conString = "INSERT_YOUR_POSTGRES_URL_HERE" //Can be found in the Details page
+
+creds.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  creds.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    creds.end();
+  });
+});
---
+++ capstone/serverSide/index.js
@@ -0,0 +1,29 @@
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const creds = require("./creds");
+const PORT = 3000;
+// var Parser = require("body-parser")
+
+// app.use(bodyParser.json())
+app.use(express.json());
+app.use(cors())
+
+
+
+app.post("/signup", (req, res) => {
+    creds.connect( async () => {
+        try {
+            const data = await creds.query(`INSERT INTO "users"(name, username, password) VALUES ('${req.body.name}', '${req.body.username}','${req.body.password}')`)
+            res.send('Created new user');
+
+        }
+        catch(err){
+            res.send(err);
+        }
+    })
+});
+
+
+app.listen(PORT, console.log(`I'm listening on ${PORT}`))
