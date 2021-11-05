5923db976e7cbd4918fbf8fbada0d3871cc9ce11
Kayla Matthews <kdmatthews77@gmail.com>
2021-11-05 10:46:44 -0400

    working front end connection

--- capstone/src/components/SignUp.js
+++ capstone/src/components/SignUp.js
@@ -6,21 +6,21 @@ export default function SignUp() {
     const [formData, setFormData] = useState({name: "", username: "", password: ""});
     console.log(formData)
 
-const newUser = async (e) => {
+
+    
+ const signUp = async (e) => {
     e.preventDefault();
 
-
-    const signUpUser = await fetch ("http://localhost:3000/signup", {
+    const signUp = await fetch ("http://localhost:3000/signup", {
         method: "POST",
         mode: "cors",
         headers: {
             'Content-Type': 'application/json'
         },
-        // body: JSON.stringify({formData}),
+        body: JSON.stringify(formData),
     })
  
-    
-}
+}  
 
     return (
         <FormDiv>
@@ -29,7 +29,7 @@ const newUser = async (e) => {
             <Input onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} name="username"type="text" placeholder="username"/>
             <Input onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} name="password"type="password" placeholder="password"/>
             {/* <Input onChange={(e)=>setFormData({...formData,[e.target.name]:e.target.value})} name="name"type="password" placeholder="verify password"/> */}
-            <FormButton onClick={(e)=>newUser(e), ()=>console.log("clicked")}>SignUp</FormButton>
+            <FormButton onClick={(e)=>signUp(e)}>SignUp</FormButton>
             <a href="/login">Already have an account? Login</a>
         </FormDiv>
     )
