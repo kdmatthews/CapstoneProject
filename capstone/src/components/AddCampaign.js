2fa6166de1a8e200cd574b9898b5b714fef280f1
Kayla Matthews <kdmatthews77@gmail.com>
2021-11-05 11:18:14 -0400

    campaign form

---
+++ capstone/src/components/AddCampaign.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AddCampaign() {
+    return (
+        <div>
+            <input type="text" placeholder="Title"/>
+            <input toString type="text" placeholder="image URL"/>
+            <input type="number" placeholder="goal" />
+            <textarea name="" id="" cols="30" rows="10" placeholder="description"></textarea>
+        </div>
+    )
+}
