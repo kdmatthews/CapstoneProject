577fda62e4746bf07bb7f4b0bf73e02249fe157a
Brande Gilbert <brandegilbert@gmail.com>
2021-11-08 12:05:17 -0500

    Bcrypt added, register/login routes updated with bcrypt

--- capstone/serverSide/creds.js
+++ capstone/serverSide/creds.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-const token = process.env.REACT_APP_elephant
-const pg = require('pg');
-const creds = new pg.Client(token);
+require("dotenv").config();
+const token = process.env.REACT_APP_elephant;
+const pg = require("pg");
+const creds = new pg.Client(
+  "postgres://wfbkcthr:NprjKixAO6W24kORo8euf6WtV_k3Q3LD@fanny.db.elephantsql.com/wfbkcthr"
+);
+
 module.exports = creds;
 
-
 // console.log(token)
-
-
