7c9924b57f4de7e0be74c238435f53bf814f83c7
brittney rawlings <bn.rawlings@gmail.com>
2021-11-08 11:04:25 -0600

    started header styling

--- capstone/src/components/NavbarData.js
+++ capstone/src/components/NavbarData.js
@@ -1,5 +1,20 @@
+import React from 'react';
+
+import * as AiIcons from "react-icons/ai";
+import * as ImIcons from "react-icons/im";
+
+
+
+
+
 export const navbarData = [
-   {name: "Home", path: "/"},
-   {name: "Login", path: "/login"},
-   {name: "Sign Up", path: "/signup"},
+   {name: "Home", 
+   path: "/",
+   icon: <AiIcons.AiFillHome/>},
+   {name: "Login", 
+   path: "/login",
+   icon:<AiIcons.AiOutlineLogin />},
+   {name: "Sign Up", 
+   path: "/signup",
+   icon: <ImIcons.ImEnter/> },
 ]
