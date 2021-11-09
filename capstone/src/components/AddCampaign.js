60a2663efa1e7f8311c9f07648be7f3a6d5a40c0
brittney rawlings <bn.rawlings@gmail.com>
2021-11-09 11:00:09 -0600

    Styling added to add campaign

--- capstone/src/components/AddCampaign.js
+++ capstone/src/components/AddCampaign.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { NewCampaignDiv } from '../styled-components/AddCampaignStyled';
+import { NewCampaignDiv, CampaignInputs, CampaignText } from '../styled-components/AddCampaignStyled';
 
 export default function AddCampaign() {
     const [campaignData, setCampaignData] = useState({creator_name: "", title: "", image: "", goal: "", description: ""});
@@ -21,13 +21,13 @@ export default function AddCampaign() {
     } 
     return (
         <NewCampaignDiv>
-             <input onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="creator_name"   type="text" placeholder="creator_name"/>
-             <input onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="image"  type="text" placeholder="image URL"/>
-            <input onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="title"  type="text" placeholder="Title"/>
+             <CampaignInputs  onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="creator_name"   type="text" placeholder="creator_name"/>
+             <CampaignInputs  onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="image"  type="text" placeholder="image URL"/>
+            <CampaignInputs  onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="title"  type="text" placeholder="Title"/>
          
-            <input onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="goal" type="number" placeholder="goal" />
+            <CampaignInputs  onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="goal" type="number" placeholder="goal" />
             
-            <textarea onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="description" name="" id="" cols="30" rows="10" placeholder="description"></textarea>
+            <CampaignText onChange={(e)=>setCampaignData({...campaignData,[e.target.name]:e.target.value})} name="description" name="" id="" cols="30" rows="10" placeholder="description"></CampaignText>
             <button onClick={(e)=>createCampaign(e)}>Add Campaign</button>
         </NewCampaignDiv>
     )
--- capstone/src/styled-components/AddCampaignStyled.js
+++ capstone/src/styled-components/AddCampaignStyled.js
@@ -3,5 +3,15 @@ import styled from "styled-components";
 export const NewCampaignDiv = styled.div`
 display: flex;
 flex-direction: column;
-
+gap: .05rem;
+align-items: center;
+height: 70vh;
+width: 80vh;
+`
+export const CampaignInputs = styled.input`
+height: 1.2rem;
+width: 60vw;
+`
+export const CampaignText = styled.textarea`
+width: 60vw;
 `
