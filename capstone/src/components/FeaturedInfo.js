84fdcb13baa8f7d4be8068be728e10b2847aa626
Kayla Matthews <kdmatthews77@gmail.com>
2021-11-12 10:55:32 -0500

    fixed

--- capstone/src/components/FeaturedInfo.js
+++ capstone/src/components/FeaturedInfo.js
@@ -2,15 +2,12 @@ import React from 'react'
 
 export default function FeaturedInfo(props) {
     const { item } = props;
-    let formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: "USD",
-    });
+  
     return (
         <div>
             <img src={item?.image} alt="" />
             <h1>{item?.title}</h1>
-            <h2>{formatter.format(item?.Goal)}</h2>
+            <h2>{item?.Goal}</h2>
             <h3>Description: {item?.Description}</h3>
             <h3>Organizer: {item?.Organizer}</h3>
         </div>
